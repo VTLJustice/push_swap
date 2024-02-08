04a0e90f64fe4ed532afa47204bedf0d289e0a26
rradules <r.n.r.2386@gmail.com>
2024-02-08 18:46:49 +0100

    Add files via upload

--- ft_normalize.c
+++ ft_normalize.c
@@ -1,13 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_normalize.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rradules <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 15:37:08 by rradules          #+#    #+#             */
+/*   Updated: 2024/02/08 18:20:19 by rradules         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
+#include <strings.h>
 
 void	ft_normalize(t_cont **stack)
 {
-	int	max_number;
-	int	min;
-	int	current;
-	int	next_min;
+	int		max_number;
+	int		min;
+	int		current;
+	int		next_min;
+	char	*pos;
+	int		i;
 	t_cont	*get_min;
 
+	i = 0;
 	max_number = 1;
 	get_min = (*stack);
 	while (get_min)
@@ -15,6 +31,11 @@ void	ft_normalize(t_cont **stack)
 		get_min = get_min->next;
 		max_number++;
 	}
+	pos = malloc(sizeof(char) * (max_number - 1));
+	if (!pos)
+		free(pos);
+	pos[max_number] = '\0';
+	memset(pos, '0', max_number - 1);
 	current = (*stack)->content;
 	get_min = (*stack);
 	while (get_min)
@@ -32,29 +53,43 @@ void	ft_normalize(t_cont **stack)
 	{
 		if ((*stack)->content == next_min)
 		{
-			(*stack)->content = current;
-			current++;
+			if (pos[i] == '0')
+			{
+				(*stack)->content = current;
+				current++;
+				pos[i] = '1';
+				i++;
+				printf("Posiciones %s\n", pos);
+			}
 			if ((*stack)->next == NULL)
 			{
-				next_min++;
 				(*stack) = get_min;
+				next_min++;
+				i = 0;
 			}
 			else
+			{
 				(*stack) = (*stack)->next;
+				i++;
+			}
 		}
 		else
 		{
 			if ((*stack)->next == NULL)
 			{
-				next_min++;
 				(*stack) = get_min;
+				next_min++;
+				i = 0;
 			}
 			else
+			{
 				(*stack) = (*stack)->next;
+				i++;
+			}
 		}
 		if (current == max_number)
 			break ;
-		printf("%d\n", next_min);
+		//printf("%d\n", next_min);
 	}
 	(*stack) = get_min;
 	ft_printstacks((*stack), (*stack));
