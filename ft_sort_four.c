ee485ef29500304a932f5ad3e3c246b97d980fc9
Rares Nicolae Radulescu <rradules@c1r12s5.42madrid.com>
2024-05-15 16:10:10 +0200

    Funciona?

--- ft_sort_four.c
+++ ft_sort_four.c
@@ -6,11 +6,54 @@
 /*   By: rradules <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 12:43:39 by rradules          #+#    #+#             */
-/*   Updated: 2024/05/13 14:56:01 by rradules         ###   ########.fr       */
+/*   Updated: 2024/05/15 15:20:16 by rradules         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	ft_first_half(t_cont **stack_a, t_cont **stack_b, int pos, int p)
+{
+	if (pos == 1)
+	{
+		p = 1;
+		ft_push(stack_b, stack_a, PB);
+	}
+	else if (pos == 2)
+	{
+		ft_swap((*stack_a), SA);
+		if (ft_check_order(*stack_a) == 1)
+		{
+			ft_push(stack_b, stack_a, PB);
+			p = 1;
+		}
+	}
+	return (p);
+}
+
+int	ft_sec_half(t_cont **stack_a, t_cont **stack_b, int pos, int p)
+{
+	if (pos == 3)
+	{
+		ft_reverse_rotate(stack_a, RRA);
+		ft_reverse_rotate(stack_a, RRA);
+		if (ft_check_order(*stack_a) == 1)
+		{
+			ft_push(stack_b, stack_a, PB);
+			p = 1;
+		}
+	}
+	else
+	{
+		ft_reverse_rotate(stack_a, RRA);
+		if (ft_check_order(*stack_a) == 1)
+		{
+			ft_push(stack_b, stack_a, PB);
+			p = 1;
+		}
+	}
+	return (p);
+}
 
 int	ft_min_pos(t_cont *stack, int min)
 {
@@ -39,39 +82,10 @@ void	ft_sort_four(t_cont **stack_a, t_cont **stack_b)
 	pushed = 0;
 	min = ft_min(*stack_a);
 	pos_min = ft_min_pos(*stack_a, min);
-	if (pos_min == 1)
-	{
-		pushed = 1;
-		ft_push(stack_b, stack_a, PB);
-	}
-	else if (pos_min == 2)
-	{
-		ft_swap((*stack_a), SA);
-		if (ft_check_order(*stack_a) == 1)
-		{
-			ft_push(stack_b, stack_a, PB);
-			pushed = 1;
-		}
-	}
-	else if (pos_min == 3)
-	{
-		ft_reverse_rotate(stack_a, RRA);
-		ft_reverse_rotate(stack_a, RRA);
-		if (ft_check_order(*stack_a) == 1)
-		{
-			ft_push(stack_b, stack_a, PB);
-			pushed = 1;
-		}
-	}
+	if (pos_min == 1 || pos_min == 2)
+		pushed = ft_first_half(stack_a, stack_b, pos_min, 0);
 	else
-	{
-		ft_reverse_rotate(stack_a, RRA);
-		if (ft_check_order(*stack_a) == 1)
-		{
-			ft_push(stack_b, stack_a, PB);
-			pushed = 1;
-		}
-	}
+		pushed = ft_sec_half(stack_a, stack_b, pos_min, 0);
 	if (ft_check_order(*stack_a) == 1)
 		ft_sort_three(stack_a);
 	if (pushed == 1)
