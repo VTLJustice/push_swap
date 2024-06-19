d1bb517290589340d0cfb8680879125a168afc7a
rradules <rradules@rradules.rradules.42madrid>
2024-06-19 17:52:55 +0200

    Un pasito pa'lante dos pa'tras

--- ft_back_to_a.c
+++ ft_back_to_a.c
@@ -11,7 +11,9 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
+int	ft_pos(t_cont *stack, int num);
+int	ft_half(t_cont *stack);
+/*
 void	ft_put_back(t_cont **stack_a, t_cont **stack_b, int pos)
 {
 	int	last_a;
@@ -89,4 +91,144 @@ void	ft_back_to_a(t_cont **stack_a, t_cont **stack_b)
 		pos = 1;
 		temp_b = (*stack_b);
 	}
+}*/
+
+void	ft_to_a(t_cont *stack_a, t_cont *stack_b, int num)
+{
+	int	diff;
+	int	cmp;
+	int	pos;
+	int	i;
+	int	half;
+
+	i = 1;
+	pos = i;
+	diff = stack_a->content - num;
+	half = ft_half(stack_a);
+	while (stack_a->next != NULL)
+	{
+		i++;
+		cmp = stack_a->next->content - num;
+		if (cmp > 0 && cmp < diff)
+			pos = i;
+		stack_a = stack_a->next;
+	}
+	if (pos >= half)
+	{
+		pos = 1 + ft_count_numbers(stack_a) - pos;
+		while (pos > 0)
+		{
+			ft_reverse_rotate(&stack_a, RA);
+			pos--;
+		}
+	}
+	else
+	{
+		while (pos > 1)
+		{
+			ft_rotate(&stack_a, RA);
+			pos--;
+		}
+	}
+	ft_push(&stack_a, &stack_b, PA);
+}
+
+void	ft_m_go(t_cont *stack_a, t_cont *stack_b, int a)
+{
+	int	i;
+	int	half;
+
+	half = ft_half(stack_a);
+	i = ft_pos(stack_a, a);
+	if (i >= half)
+	{
+		i = 1 + ft_count_numbers(stack_a) - i;
+		while (i > 0)
+		{
+			ft_reverse_rotate(&stack_a, RA);
+			i--;
+		}
+	}
+	else
+	{
+		while (i > 1)
+		{
+			ft_rotate(&stack_a, RA);
+			i--;
+		}
+	}
+	ft_push(&stack_a, &stack_b, PA);
+}
+
+void	ft_sort_min(t_cont **stack_a)
+{
+	int	min;
+	int	half;
+	int	i;
+	
+	min = ft_min(*stack_a);
+	i = ft_pos(*stack_a, min);
+	half = ft_half(*stack_a);
+	if (i >= half)
+	{
+		i = 1 + ft_count_numbers(*stack_a) - i;
+		while (i > 0)
+		{
+			ft_reverse_rotate(stack_a, RA);
+			i--;
+		}
+	}
+	else
+	{
+		while (i > 1)
+		{
+			ft_reverse_rotate(stack_a, RA);
+			i--;
+		}
+	}
+}
+
+void	ft_move_to_a(t_cont *stack_a, t_cont *stack_b, int chosen, int *edges)
+{
+	int	a;
+
+	a = stack_a->content;
+	if (chosen > edges[1])
+	{
+		a = edges[0];
+		ft_m_go(stack_a, stack_b, a);
+	}
+	else if (chosen > edges[0])
+	{
+		a = ft_max(stack_a);
+		ft_m_go(stack_a, stack_b, a);
+	}
+	else
+		ft_to_a(stack_a, stack_b, chosen);
+}
+
+void	ft_back_to_a(t_cont **stack_a, t_cont **stack_b)
+{
+	int	*edges;
+	t_cont	*temp_a;
+	t_cont	*temp_b;
+	
+	temp_b = (*stack_b);
+	temp_a = (*stack_a);
+	edges = malloc(sizeof(int) * 2);
+	if (!edges)
+		exit(-1);
+	edges[0] = ft_max(*stack_a);
+	edges[1] = (*stack_a)->content;
+	ft_printf("aqui\n");
+	ft_printstacks(*stack_a, *stack_b);
+	while (ft_count_numbers(*stack_b) > 0)
+	{
+		ft_move_to_a(temp_a, temp_b, temp_b->content, edges);
+		temp_b = temp_b->next;
+		edges[0] = ft_max(*stack_a);
+		edges[1] = ft_min(*stack_a);
+	}
+	if (ft_check_order(*stack_a) == 1)
+		ft_sort_min(stack_a);
 }
--- ft_start_alg.c
+++ ft_start_alg.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
+/*
 int	ft_find_pos_b(t_cont *stack_b, int num, int max, int min)
 {
 	int	i;
@@ -271,6 +271,242 @@ void	ft_first_steps(t_cont **stack_a, t_cont **stack_b)
 		min = ft_min(*stack_b);
 		max = ft_max(*stack_b);
 	}
+}
+*/
+int	ft_pos(t_cont *stack, int num)
+{
+	int	i;
+
+	i = 1;
+	while (stack->content != num)
+	{
+		i++;
+		stack = stack->next;
+	}
+	return (i);
+}
+int	ft_half(t_cont *stack)
+{
+	int	i;
+
+	if (ft_count_numbers(stack) % 2 == 0)
+		i = ft_count_numbers(stack) / 2;
+	else
+		i = ft_count_numbers(stack) / 2 + 1;
+	return (i);
+}
+
+void	ft_do_steps(t_cont **stack_a, t_cont **stack_b, int *num, int *edges)
+{
+	int	half_a;
+	int	half_b;
+	int	pos_a;
+	int	pos_b;
+
+	pos_a = ft_pos(*stack_a, num[0]);
+	if (num[0] > edges[0])
+		pos_b = ft_pos(*stack_b, edges[0]);
+	else if (num[0] < edges[1])
+		pos_b = ft_pos(*stack_b, edges[0]);
+	else
+		pos_b = ft_pos(*stack_b, num[1]);
+	half_a = ft_half(*stack_a);
+	half_b = ft_half(*stack_b);
+	if (pos_a >= half_a && pos_b >= half_b)
+	{
+		pos_a = 1 + ft_count_numbers(*stack_a) - pos_a;
+		pos_b = 1 + ft_count_numbers(*stack_b) - pos_b;
+		while (pos_a > 0 && pos_b > 0)
+		{
+			if (pos_a > pos_b)
+			{
+				ft_reverse_rotate(stack_a, RA);
+				pos_a--;
+			}
+			else if (pos_a < pos_b)
+			{
+				ft_reverse_rotate(stack_b, RB);
+				pos_b--;
+			}
+			else
+			{
+				ft_rr_rotate(stack_a, stack_b);
+				pos_a--;
+				pos_b--;
+			}
+		}
+		ft_push(stack_b, stack_a, PB);
+	}
+	else if (pos_a <= half_a && pos_b <= half_b)
+	{
+		while (pos_a > 1 && pos_b > 1)
+		{
+			if (pos_a > pos_b)
+			{
+				ft_rotate(stack_a, RA);
+				pos_a--;
+			}
+			else if (pos_a < pos_b)
+			{
+				ft_rotate(stack_b, RB);
+				pos_a--;
+			}
+			else
+			{
+				ft_rrotate(stack_a, stack_b);
+				pos_a--;
+				pos_b--;
+			}
+		}
+		ft_push(stack_b, stack_a, PB);
+	}
+	else if (pos_a >= half_a && pos_b <= half_b)
+	{	
+		pos_a = 1 + ft_count_numbers(*stack_a) - pos_a;
+		while (pos_a > 0)
+		{
+			ft_reverse_rotate(stack_a, RRA);
+			pos_a--;
+		}
+		while (pos_b > 1)
+		{
+			ft_rotate(stack_b, RB);
+			pos_b--;
+		}
+		ft_push(stack_b, stack_a, PB);
+	}
+	else
+	{
+		pos_b = 1 + ft_count_numbers(*stack_b) - pos_b;
+		while (pos_b > 0)
+		{
+			ft_reverse_rotate(stack_b, RRB);
+			pos_b--;
+		}
+		while (pos_a > 1)
+		{
+			ft_rotate(stack_a, RA);
+			pos_a--;
+		}
+		ft_push(stack_b, stack_a, PB);
+	}
+}
+
+int	ft_count_steps(t_cont *stack_a, t_cont *stack_b, int a, int b)
+{
+	int	i;
+	int	j;
+	int	nums_a;
+	int	nums_b;
+
+	nums_a = ft_count_numbers(stack_a);
+	nums_b = ft_count_numbers(stack_b);
+	i = ft_pos(stack_a, a);
+	j = ft_pos(stack_b, b);
+	if (i >= ft_half(stack_a))
+		i = nums_a - i;
+	if (j >= ft_half(stack_b))
+		j = nums_b - i;
+	if (i == j)
+		return (i);
+	else if (i > j)
+		return (i);
+	else
+		return (j);
+}
+
+int	*ft_find_steps(t_cont *stack_a, t_cont *stack_b, int *num, int *edge)
+{
+	int	diff;
+	int	stp;
+	int	cmp;
+	int	*a;
+	t_cont	*head;
+
+	head = stack_b;
+	diff = 0;
+	edge = edge;
+	num[0] = stack_a->content;
+	num[1] = stack_b->content;
+	a = malloc(sizeof(int) * 2);
+	while (stack_a)
+	{
+		while (stack_b->next != NULL)
+		{
+			ft_printf("stack_a->content = %d\n", stack_a->content);
+			if (stack_a->content > edge[0] || stack_a->content < edge[1])
+			{
+				a[0] = edge[0];
+				break ;
+			}
+			diff = stack_a->content - stack_b->content;
+			if (diff > (stack_a->content - stack_b->next->content) ||
+					diff < 0)
+				a[0] = stack_b->content;
+			stack_b = stack_b->next;
+		}
+		stack_b = head;
+		stp = ft_count_steps(stack_a, stack_b, stack_a->content, a[0]);
+		ft_printf("stp = %d, stk_a = %d a[0] = %d\n", stp, stack_a->content, a[0]);
+		if (stack_a->next != NULL)
+			stack_a = stack_a->next;
+		while (stack_b->next != NULL)
+		{
+			if (stack_a->content > edge[0] || stack_a->content < edge[1])
+			{
+				a[1] = edge[0];
+				break ;
+			}
+			diff = stack_a->content - stack_b->content;
+			if (diff > stack_a->content - stack_b->next->content ||
+					diff < 0)
+				a[1] = stack_b->content;
+			stack_b = stack_b->next;
+		}
+		stack_b = head;
+		cmp = ft_count_steps(stack_a, stack_b, stack_a->content, a[1]);
+		ft_printf("cmp = %d, stk_a = %d a[1] = %d\n", cmp, stack_a->content, a[1]);
+		if (stp > cmp)
+		{
+			num[0] = stack_a->content;
+			num[1] = a[1];
+		}
+		stack_a = stack_a->next;
+	}
+	free(a);
+	ft_printf("num_a = %d, num_b = %d\n", num[0], num[1]);
+	return (num);
+}
+
+void	ft_first_steps(t_cont **stack_a, t_cont **stack_b)
+{
+	int	*chosen;
+	int	*max_min;
+
+	chosen = malloc(sizeof(int) * 2);
+	if (!chosen)
+		exit(-1);
+	max_min = malloc(sizeof(int) * 2);
+	if (!max_min)
+		exit(-1);
+	max_min[0] = (*stack_a)->content;
+	ft_push(stack_b, stack_a, PB);
+	if (max_min[0] < (*stack_a)->content)
+		max_min[0] = (*stack_a)->content;
+	else
+		max_min[1] = (*stack_a)->content;
+	ft_push(stack_b, stack_a, PB);
+	ft_printstacks(*stack_a, *stack_b);
+	while (ft_count_numbers(*stack_a) > 3)
+	{
+		chosen = ft_find_steps(*stack_a, *stack_b, chosen, max_min);
+		ft_do_steps(stack_a, stack_b, chosen, max_min);
+		max_min[0] = ft_max(*stack_b);
+		max_min[1] = ft_min(*stack_b);
+		ft_printstacks(*stack_a, *stack_b);
+	}
+	free(chosen);
+	free(max_min);
 }
 
 void	ft_start_alg(t_cont **stack)
